3bbb3f1ddb263d8fb2ccaf180d6c03f50d03764c
Sara Romão <sara-romao@live.com>
2020-03-30 18:37:24 -0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+var n = parseInt(prompt("Informe um número", "5"));
+var elemento = (
+    <div>
+        <h1>Tabuada do número {n}</h1>
+        <h3>1 X {n} = {1*n}</h3>
+        <h3>2 X {n} = {2*n}</h3>
+        <h3>3 X {n} = {3*n}</h3>
+        <h3>4 X {n} = {4*n}</h3>
+        <h3>5 X {n} = {5*n}</h3>
+        <h3>6 X {n} = {6*n}</h3>
+        <h3>7 X {n} = {7*n}</h3>
+        <h3>8 X {n} = {8*n}</h3>
+        <h3>9 X {n} = {9*n}</h3>
+        <h3>10 X {n} = {10*n}</h3>
+
+    </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(elemento, document.getElementById('root')
+);
